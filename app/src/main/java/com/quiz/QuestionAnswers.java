f9144377ee33708b4c28cb354c610b1b35e7a20e
TasheeWangc <tashiwangchuk619@gmail.com>
2023-08-21 23:32:25 +0530

    initial commit

---
+++ app/src/main/java/com/quiz/QuestionAnswers.java
@@ -0,0 +1,24 @@
+package com.quiz;
+
+public class QuestionAnswers {
+    public static String question[] ={
+            "Which of these is not a computer language?",
+            "Who coined the term \"artificial intelligence\"?",
+            "What does the acronym VPN stand for?"
+
+    };
+    public static String choices[][]={
+            {"HyperTalk","Windows 2000","Ada","COBOL"},
+            {"Donald Ervin Knuth","Charles Bachman","John McCarthy","Herbert A. Simon"},
+            {"visual protocol network","virtual private network","visual protocol networking","visual processing network"}
+    };
+
+    public static String correctAnswer[]={
+            "Windows 2000",
+            "John McCarthy",
+            "virtual private network"
+
+
+    };
+
+}
